75a4803cf94b0723579d666ff64ac0dded6ec457
sudo-apt-nauak <kauancc.unio@gmail.com>
2025-08-18 13:13:20 -0300

    Objetos podem ser criado no banco

--- index.js
+++ index.js
@@ -12,10 +12,43 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 //iniciando o express
 const app = express();
 
+const Person = require("./models/Person");
+
 //criar uma forma de ler o JSON
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
+
+//rotas da API
+//a função async permite que eu use o await dentro dela
+app.post("/person", async (req, res) => {
+  //req.body vem do JSON que foi enviado
+  const { name, salary, approved } = req.body;
+
+  //criando uma pessoa
+  const person = {
+    name,
+    salary,
+    approved,
+  };
+
+  //Método create do mongoose
+  //ele cria uma nova pessoa no banco de dados
+  //await é usado para esperar a resposta do banco de dados
+  //se não usar o await, o código continua executando sem esperar a resposta
+  //e pode dar erro
+  //try/catch é usado para tratar erros
+  //se der erro, o código não vai quebrar, e sim retornar um erro
+  //se der certo, o código continua executando normalmente
+  try {
+    //criando a pessoa no banco de dados
+    await Person.create(person);
+
+    res.status(201).json({ message: "Pessoa inserida com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
 //rota inicial
 app.get("/", (req, res) => {
