dfcf0b2620df8a23409955ae7b39739fb347a00e
sudo-apt-nauak <kauancc.unio@gmail.com>
2025-08-18 18:06:28 -0300

    Versão para apresentação

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 //Dessa forma eu deixo varáiveis de ambiente mais seguras
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_USER = "kauanccunio";
+const DB_PASSWORD = encodeURIComponent("PGQfQ2w2ucCJ6pG2");
 
 //iniciando o express
 const app = express();
-
-const Person = require("./models/Person");
 
 //criar uma forma de ler o JSON
 app.use(express.urlencoded({ extended: true }));
@@ -20,35 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //rotas da API
-//a função async permite que eu use o await dentro dela
-app.post("/person", async (req, res) => {
-  //req.body vem do JSON que foi enviado
-  const { name, salary, approved } = req.body;
-
-  //criando uma pessoa
-  const person = {
-    name,
-    salary,
-    approved,
-  };
-
-  //Método create do mongoose
-  //ele cria uma nova pessoa no banco de dados
-  //await é usado para esperar a resposta do banco de dados
-  //se não usar o await, o código continua executando sem esperar a resposta
-  //e pode dar erro
-  //try/catch é usado para tratar erros
-  //se der erro, o código não vai quebrar, e sim retornar um erro
-  //se der certo, o código continua executando normalmente
-  try {
-    //criando a pessoa no banco de dados
-    await Person.create(person);
-
-    res.status(201).json({ message: "Pessoa inserida com sucesso!" });
-  } catch (error) {
-    res.status(500).json({ error: error });
-  }
-});
+const personRoutes = require("./routes/personRoutes");
+app.use("/person", personRoutes);
 
 //rota inicial
 app.get("/", (req, res) => {
